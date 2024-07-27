760109afc1d9d135ef08716a262d6965b804cba2
jud3v2 <judikaelbellance@icloud.com>
2024-07-27 18:58:17 +0200

    ajout de la fonction pour la gestion des relation

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -20,4 +20,27 @@ class Product extends Model
         'item_id',
         "stock"
     ];
+
+    public function getRelatedItem()
+    {
+        if($this->type === 'skin') {
+            return $this->hasOne(Skin::class, 'id', 'item_id');
+        } elseif($this->type === "sticker") {
+            return $this->hasOne(Stickers::class, 'id', 'item_id');
+        } elseif($this->type === "music-kit") {
+            return $this->hasOne(MusicKits::class, 'id', 'item_id');
+        } elseif($this->type === "graffiti") {
+            return $this->hasOne(Graffiti::class, 'id', 'item_id');
+        } elseif($this->type === "patch") {
+            return $this->hasOne(Patches::class, 'id', 'item_id');
+        } elseif($this->type === "collectible") {
+            return $this->hasOne(Collectible::class, 'id', 'item_id');
+        } elseif($this->type === "key") {
+            return $this->hasOne(Keys::class, 'id', 'item_id');
+        } elseif($this->type === "crate") {
+            return $this->hasOne(Crates::class, 'id', 'item_id');
+        }
+
+        return null;
+    }
 }
