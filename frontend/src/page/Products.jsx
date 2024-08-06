a5a99e26c70326673a76b48798f3662f6043d3e7
Maxime <maxime.caron@epitech.eu>
2024-08-06 15:34:08 +0200

    [*] Fix d'un bug qui empêcher le bon fonctionnant des filtres par catégories et skins.

--- frontend/src/page/Products.jsx
+++ frontend/src/page/Products.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import ProductCard from '../components/ProductCard';
 import Dropdown from '../components/Dropdown';
 import Loading from "../components/Loading.jsx";
-import { Slider } from '@mui/material'; // Import MUI Slider
 import '../index.css';
 
 const Products = () => {
@@ -14,8 +13,8 @@ const Products = () => {
   const [selectedSkin, setSelectedSkin] = useState('');
   const [skinOptions, setSkinOptions] = useState([]);
   const [categoryOptions, setCategoryOptions] = useState([]);
-  const [priceRange, setPriceRange] = useState([0, 10000]); // Default range
-  const [maxPrice, setMaxPrice] = useState(10000); // Separate state for max price
+  const [priceRange, setPriceRange] = useState([0, 10000]);
+  const [maxPrice, setMaxPrice] = useState(10000);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -32,8 +31,8 @@ const Products = () => {
 
         // Calculate the maximum price from the fetched products
         const maxPrice = Math.max(...fetchedProducts.map(product => product.price), 0);
-        setMaxPrice(maxPrice); // Set the max price
-        setPriceRange([0, maxPrice]); // Set the price range to [0, maxPrice]
+        setMaxPrice(maxPrice);
+        setPriceRange([0, maxPrice]);
       } catch (error) {
         console.error('Error fetching products:', error);
         setError(error);
@@ -123,9 +122,31 @@ const Products = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {products
             .filter(product => {
+              const skin = product.skin;
+              const categoryString = skin?.category;
+              const weaponsString = skin?.weapons;
+              let category = null;
+              let weapon = null;
+
+              if (categoryString) {
+                try {
+                  category = JSON.parse(categoryString);
+                } catch (error) {
+                  console.error('Error parsing category:', error);
+                }
+              }
+
+              if (weaponsString) {
+                try {
+                  weapon = JSON.parse(weaponsString);
+                } catch (error) {
+                  console.error('Error parsing weapons:', error);
+                }
+              }
+
               const matchesSearch = product.name.toLowerCase().includes(searchTerm.toLowerCase());
-              const matchesCategory = selectedCategory ? product.category.toLowerCase() === selectedCategory.toLowerCase() : true;
-              const matchesSkin = selectedSkin ? product.skin.toLowerCase() === selectedSkin.toLowerCase() : true;
+              const matchesCategory = selectedCategory ? category?.name.toLowerCase() === selectedCategory.toLowerCase() : true;
+              const matchesSkin = selectedSkin ? weapon?.name.toLowerCase() === selectedSkin.toLowerCase() : true;
               const matchesPrice = product.price >= priceRange[0] && product.price <= priceRange[1];
 
               return matchesSearch && matchesCategory && matchesSkin && matchesPrice;
