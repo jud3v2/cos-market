368bfa86e15a659eed1e414540590ab9ff341223
Maxime <maxime.caron@epitech.eu>
2024-08-11 12:53:09 +0200

    [+] Ajout d'une puce afin d'afficher le nombre d'item dans le panier de l'utilisateur (à rendre dynamique plus tard).

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import logo from '../assets/logo/logo_cosmarket.png';
 import '../index.css';
 
 const Header = () => {
   const location = useLocation();
+  const [cartItemCount, setCartItemCount] = useState(0);
+
+  useEffect(() => {
+    const updateCartItemCount = () => {
+      const cart = JSON.parse(localStorage.getItem('cart')) || [];
+      setCartItemCount(cart.length);
+    };
+
+    updateCartItemCount();
+
+    window.addEventListener('storage', updateCartItemCount);
+
+    return () => {
+      window.removeEventListener('storage', updateCartItemCount);
+    };
+  }, []);
 
   const getNavItemClass = (path) => {
     return location.pathname === path ? 'nav-item border-b-4 border-gray-950 ' : 'nav-item';
@@ -25,7 +41,7 @@ const Header = () => {
           </div>
           <div className="flex flex-col xl:gap-32 lg:gap-20 lg:flex-row md:flex-col sm:flex-col items-center order-3 sm:order-3 sm:space-x-6">
             <a href="/profil" className={getNavItemClass('/profil')}>PROFIL</a>
-            <a href="/panier" className={getNavItemClass('/panier')}>PANIER</a>
+            <a href="/panier" className={getNavItemClass('/panier')}>PANIER ({cartItemCount})</a>
           </div>
         </nav>
       </header>
--- frontend/src/page/Panier.jsx
+++ frontend/src/page/Panier.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import CartItem from "../components/CartItem";
 import CartService from "../services/cartService";
-import Footer from "../components/Footer";
 import "../index.css";
 import { Link } from "react-router-dom";
 
