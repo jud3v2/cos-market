92bc7c9f4a37b2676e9c02e267010c55f02aa442
sofiane <sofiane.bessaha@epitech.eu>
2024-08-21 14:14:05 +0200

    [+]Ajout de la bonne récupération de l'inventaire csgo directement en cliquand sur le bouton

--- frontend/src/views/ProfilPage.jsx
+++ frontend/src/views/ProfilPage.jsx
@@ -3,31 +3,35 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
 const ProfilePage = () => {
-    const [profileUrl, setProfileUrl] = useState('');
+    const [steamId, setSteamId] = useState('');
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchProfileUrl = async () => {
+        const fetchSteamId = async () => {
             try {
                 const user = jwtDecode(localStorage.getItem('token'));
-                const response = await axios.get('http://localhost:8000/api/steam/profile-url?user_id='+ user.sub);
-                setProfileUrl(response.data.profile_url);
+                const response = await axios.get('http://localhost:8000/api/steam/profile-url?user_id=' + user.sub);
+                const profileUrl = response.data.profile_url;
+
+                const steamId = profileUrl.split('/').filter(Boolean).pop();
+                setSteamId(steamId);
             } catch (err) {
                 setError(err.message);
             }
         };
 
-        fetchProfileUrl();
+        fetchSteamId();
     }, []);
 
     if (error) return <p>Error: {error}</p>;
+    const inventoryUrl = `https://steamcommunity.com/profiles/${steamId}/inventory/#730`;
 
     return (
         <div>
             <h1>Your Steam Profile</h1>
-            {profileUrl ? (
-                <a href={profileUrl} target="_blank" rel="noopener noreferrer">
-                    <button>View Your Steam Profile</button>
+            {steamId ? (
+                <a href={inventoryUrl} target="_blank" rel="noopener noreferrer">
+                    <button>View Your CS:GO Inventory</button>
                 </a>
             ) : (
                 <p>Loading...</p>
@@ -37,3 +41,4 @@ const ProfilePage = () => {
 };
 
 export default ProfilePage;
+
