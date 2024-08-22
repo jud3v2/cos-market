789fa703c2a986d728e5874f053f059d9c0641a9
Flo <florian.desitter@epitech.eu>
2024-08-22 11:44:38 +0200

    [+] Design de la page profil

--- frontend/src/views/ProfilPage.jsx
+++ frontend/src/views/ProfilPage.jsx
@@ -29,29 +29,30 @@ const ProfilePage = () => {
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <div>
-            {userProfile ? (
-                <div>
-                    <img src={userProfile.avatar} alt="Steam Avatar" style={{ borderRadius: '50%', width: '150px', height: '150px' }} />
-                    <h2>{userProfile.profile_name}</h2>
-                    <p><strong>Pays :</strong> {userProfile.country || 'Unknown'}</p>
-                    <p><strong>Adresse email :</strong> {userProfile.email || 'Unknown'}</p>
-                    <p><strong>Steam ID :</strong> {userProfile.steam_id || 'Unknown'}</p>
-                    <a href={`${userProfile.profile_url}/inventory/#730`} target="_blank" rel="noopener noreferrer">
-                        <button className="bg-yellow-400 hover:bg-orange-400 text-white text-xl font-bold py-3 px-52 rounded">
-                            Voir mon inventaire Counter Strike 2
-                        </button>
-                    </a>
-                    <a href={userProfile.profile_url} target="_blank" rel="noopener noreferrer">
-                        <button className="flex mt-2 bg-yellow-400 hover:bg-orange-400 text-white text-xl font-bold py-3 px-52 rounded">
-                            Voir mon profil Steam
-                        </button>
-                    </a>
-                </div>
-            ) : (
-                <p>Loading...</p>
-            )}
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    {userProfile ? (
+        <div className="flex flex-col items-center w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
+            <img src={userProfile.avatar} alt="Steam Avatar" className="rounded-full mb-4" style={{ width: '150px', height: '150px' }} />
+            <h2 className="text-2xl font-bold mb-2">{userProfile.profile_name}</h2>
+            <p className="text-gray-600 mb-2"><strong>Pays :</strong> {userProfile.country || 'Inconnu'}</p>
+            <p className="text-gray-600 mb-2"><strong>Adresse email :</strong> {userProfile.email || 'Inconnu'}</p>
+            <p className="text-gray-600 mb-4"><strong>Steam ID :</strong> {userProfile.steam_id || 'Inconnu'}</p>
+            <a href={`${userProfile.profile_url}/inventory/#730`} target="_blank" rel="noopener noreferrer">
+                <button className="bg-yellow-400 hover:bg-orange-400 text-white text-xl font-bold py-3 px-10 rounded-lg mb-4">
+                    Voir mon inventaire Counter Strike 2
+                </button>
+            </a>
+            <a href={userProfile.profile_url} target="_blank" rel="noopener noreferrer">
+                <button className="bg-yellow-400 hover:bg-orange-400 text-white text-xl font-bold py-3 px-10 rounded-lg">
+                    Voir mon profil Steam
+                </button>
+            </a>
         </div>
+    ) : (
+        <p>Loading...</p>
+    )}
+</div>
+
     );
 };
 
