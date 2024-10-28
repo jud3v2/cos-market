d3987014599b247655e07460e2dec26460b5835f
jud3v2 <judikaelbellance@icloud.com>
2024-10-28 12:06:25 +0100

    [+] Miise à jour de la configuration afin de savoir si cela fonctionne bien

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-        backendUrl: "http://localhost:8000/api",
-        frontendUrl: "http://localhost:5173",
+        backendUrl: "https://cos-market-backend.jud3v.fr/api",
+        frontendUrl: "https://cos-market.jud3v.fr",
         stripePublicKey: "pk_test_51MloA8GU8a3DiICfutZedAE7wwxrf9PUXZkCflMwo1EhVv4is94cD558xufFjYOiFIo9xmRzf6wcZGHBrapWgpVv00OCQoijqc",
         stripeSecretKey: "sk_test_51MloA8GU8a3DiICfAEZGBqlwjHXbXGndJ0hpLAwQJjxgAzxmMvNC91xPtqByiLAnUHrUUyZ00fLIf6BxD9baa3IH00tGBWgflA",
 }
